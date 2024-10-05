b739f533a6ffb00664f0ff6dd720d11ff894d7bf
Cameron Suess <camsuess@grouper.cs.colostate.edu>
2024-10-05 09:04:43 -0600

    Fixes #15: added parse_args method and args for ip, port, and help

--- client.py
+++ client.py
@@ -9,4 +9,13 @@ class GameClient:
         self.sel = selectors.DefaultSelector()
         self.host = host
         self.port = port
-  
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Trivia Game Client', add_help=False)
+    
+    parser.add_argument('i', '--ip', type=str, help='Server IP')
+    parser.add_argument('-p', '--port', type=int, required=True, help='Server Port')
+    parser.add_argument('h', '--help', action=help, help='Show help message and exit')
+    
+    return parser.parse_args()
