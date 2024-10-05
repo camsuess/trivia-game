ed034d893f0261ee8c2608bfabf55c94dc1c3e81
Cameron Suess <camsuess@grouper.cs.colostate.edu>
2024-10-05 09:15:38 -0600

    Fixes #9: created method for fetching questions from api

--- server.py
+++ server.py
@@ -6,12 +6,21 @@ import selectors
 import types
 import argparse
 
+API_URL = 'https://opentdb.com/api.php?amount=1&type=boolean'
+
 class GameServer:
     def __init__(self, host, port):
         self.sel = selectors.DefaultSelector()
         self.host = host
         self.port = port
         self.clients = set()
+        self.question = self.fetch_question()
+        
+    def fetch_question(self):
+        response = requests.get(API_URL)
+        question = response.json().get('results', [])
+        logging.info(f'Fetched {len(question)} trivia questions.')
+        return question
         
 
 def parse_args():
