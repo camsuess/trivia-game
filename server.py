c8b08c35ca3a76955fd15a33c5b2a780c6e87497
Cameron Suess <camsuess@grouper.cs.colostate.edu>
2024-10-19 10:39:01 -0600

    Fixes #42: implemented a basic message class

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import requests
 import socket
 import selectors
 import argparse
+import message
 
 API_URL = 'https://opentdb.com/api.php?amount=1&type=boolean'
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
@@ -47,12 +48,21 @@ class GameServer:
             data = {
             'question': self.question['question'],
             'choices': self.question['incorrect_answers'] + [self.question['correct_answer']],
-            'correct_answer': self.question['correct_answer']
             }
-            conn.send(json.dumps(data).encode('utf-8'))
+            for client_conn in self.clients:
+                message.send(client_conn, data)
+    
+    def process_answer(self, conn, data):
+        if data['answer'] == self.question['correct_answer']:
+            response = "Correct!"
+            self.clients[conn]['score'] += 1
+        else:
+            response = f"Wrong! The correct answer was {self.question['correct_answer']}."
+        
+        self.notify_all(f"{self.clients[conn]['name']} answered: {response}")
             
     def send_name_prompt(self, conn):
-        conn.send(json.dumps({"message": "Please enter your name:"}).encode('utf-8'))
+        message.send(conn, {"message": "Please enter your name:"})
         
     def notify_all(self, message, exclude_conn=None):
         for conn in self.clients:
