49aab9e72da1f62383d4cc26d7e84bf95022ab8b
Cameron Suess <camsuess@grouper.cs.colostate.edu>
2024-11-03 09:05:10 -0700

    Fixes #89: separated process request if statements into methods

--- client.py
+++ client.py
@@ -31,57 +31,70 @@ class GameClient:
             logging.debug(f"Received request: {request}")
 
             if request['action'] == "set_name":
-                username = input(request['message'])
-                response_message = {
-                    "action": "set_name",
-                    "name": username
-                }
-                Message.send(self.client_socket, response_message)
-            elif request['action'] == "question":
-                print("Question:", request['question'])
-                answer = input("Your answer (true/false): ")
-                answer_message = {
-                    "action": "answer",
-                    "answer": answer
-                }
-                Message.send(self.client_socket, answer_message)
-            elif request['action'] == "answer_feedback":
-                feedback = request.get('message')
-                if feedback == "Invalid answer format. Please reply with 'True' or 'False'.":
-                    print(feedback)
-                    answer = input("\nYour answer (true/false): ")
-                    answer_message = {
-                        "action": "answer",
-                        "answer": answer
-                    }
-                    Message.send(self.client_socket, answer_message)
-                else:
-                    print(request['message'])
-                    print(f"Your current score: {request['score']}\n")
-            # move this inside of logic to send confirmation to server and then get question?
-            elif request['action'] == "score_update":
-                print('Current game scores:')
-                for player, score in request['scores'].items():
-                    print(f'{player}: {score}')
-
+                self.set_name(request)
             
+            elif request['action'] == "question":
+                self.question_response(request)
+            
+            elif request['action'] == "answer_feedback":
+                self.process_answer_feedback(request)
+            
+            elif request['action'] == "score_update":
+                self.process_score_update(request)
+
+    def set_name(self, request):
+        username = input(request['message'])
+        response_message = {
+            "action": "set_name",
+            "name": username
+        }
+        Message.send(self.client_socket, response_message)
+    
+    def question_response(self, request):
+        print("Question:", request['question'])
+        answer = input("Your answer (true/false): ")
+        answer_message = {
+            "action": "answer",
+            "answer": answer
+        }
+        Message.send(self.client_socket, answer_message)
+    
+    def process_answer_feedback(self, request):
+        feedback = request.get('message')
+        if feedback == "Invalid answer format. Please reply with 'True' or 'False'.":
+            print(feedback)
+            answer = input("\nYour answer (true/false): ")
+            answer_message = {
+                "action": "answer",
+                "answer": answer
+            }
+            Message.send(self.client_socket, answer_message)
+        else:
+            print(request['message'])
+            print(f"Your current score: {request['score']}\n")
+    
+    def process_score_update(self, request):
+        print('Current game scores:')
+        for player, score in request['scores'].items():
+            print(f'{player}: {score}')
+    
     def start(self):
         try:
             while True:
-                events = client.sel.select()
+                events = self.sel.select()
                 for key, _ in events:
                     callback = key.data
                     callback(key.fileobj, key.events)
         except KeyboardInterrupt:
             logging.info("\nClient shutting down.")
-            self.notify_disconnect(client)
+            self.notify_disconnect()
         finally:
-            client.close()
+            self.close()
     
     def close(self):
         self.client_socket.close()
         
-    def notify_disconnect(self, sock):
+    def notify_disconnect(self):
         message = {
             "action": "disconnect"
         }
@@ -90,7 +103,6 @@ class GameClient:
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Trivia Game Client', add_help=False)
-    
     parser.add_argument('-i', '--ip', type=str, default='127.0.0.1', help='Server IP')
     parser.add_argument('-p', '--port', type=int, required=True, help='Server Port')
     parser.add_argument('-h', '--help', action='help', help='Show help message and exit')
