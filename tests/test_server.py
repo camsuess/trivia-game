c9fbee55d5d97b0217e503dc4dc625993e66e82a
Cameron Suess <camsuess@grouper.cs.colostate.edu>
2024-12-04 09:51:48 -0700

    added tests for server

--- tests/test_server.py
+++ tests/test_server.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import unittest
 from unittest.mock import patch, MagicMock
-from app.server import GameRoom
+from app.server import GameRoom, GameServer, Player
 
 class TestGameRoom(unittest.TestCase):
 
@@ -15,6 +15,26 @@ class TestGameRoom(unittest.TestCase):
         self.gr.players.append("dave")
         self.gr.players.append("tim")
         self.assertTrue(self.gr.is_full())
+
+class TestGameServer(unittest.TestCase):
+
+    @patch('socket.socket')
+    def setUp(self, mock_socket):
+        self.mock_server_socket = MagicMock()
+        mock_socket.return_value = self.mock_server_socket
+        self.server = GameServer('127.0.0.1', 12345, 2)
+
+    def tearDown(self):
+        self.server.server_socket.close()
+
+    def test_create_server_socket(self):
+        self.mock_server_socket.bind.assert_called_once_with(('127.0.0.1', 12345))
+        self.mock_server_socket.listen.assert_called_once()
+
+    def test_send_message(self):
+        mock_player = Player(MagicMock(), ('127.0.0.1', 12345))
+        self.server.send_message(mock_player, {"action": "test", "message": "Hello World"})
+        self.assertIn(b'{"action": "test", "message": "Hello World"}', mock_player.send_buffer)
 
 if __name__ == "__main__":
     unittest.main()
